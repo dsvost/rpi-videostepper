3df5cff3598f8871e4c6172c2dc9b3d7be4f3711
Dmitriy Vostrikov <dsvost@gmail.com>
2016-02-24 01:01:42 +0300

    License preambule fix

--- serialomx.py
+++ serialomx.py
@@ -2,9 +2,9 @@
 
 # Copyright 2015-2016 Dmitriy Vostrikov <dsvost@gmail.com>
 #
-# This file is part of RPiVideoStepper.
+# This file is part of rpi-videostepper.
 #
-# RPiVideoStepper is free software: you can redistribute it and/or modify
+# rpi-videostepper is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
